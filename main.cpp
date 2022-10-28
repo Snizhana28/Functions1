c18a7715e7d38828889f45e0fb8f9f1a0eedf652
snizhana28 <snizhana2812kostyuk@gmail.com>
2022-10-28 13:17:56 +0300

    dev: update main.cpp -> add main func

---
+++ main.cpp
@@ -0,0 +1,10 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	return 0;
+}
